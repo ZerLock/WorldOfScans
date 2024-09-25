fc154b05193a52b31ec0e00229e86af8e4e48e98
Léo Dubosclard <leo.dubosclard@epitech.eu>
2024-09-25 20:39:15 +0900

    add backend to get chapters without cors from navigator

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const port = 8080;
+
+app.use(cors());
+
+function sanitizeMangaName(mangaName) {
+    return mangaName.replaceAll('%20', '-').replaceAll(' ', '-').toLowerCase();
+}
+
+app.get('/manga/:mangaName', async (req, res) => {
+  const mangaName = req.params.mangaName;
+
+  const url = `https://anime-sama.fr/catalogue/${sanitizeMangaName(mangaName)}/scan/vf/episodes.js?filever=615193`;
+
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Le serveur tourne sur http://localhost:${port}`);
+});
