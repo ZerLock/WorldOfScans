4b2512c6ef1a75646e9408e851c3db6baa2d457f
Léo Dubosclard <leo.dubosclard@epitech.eu>
2024-10-10 15:23:46 +0900

    remove useless debug log

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,15 +12,12 @@ function sanitizeMangaName(mangaName) {
 
 app.get('/manga/:mangaName', async (req, res) => {
   const mangaName = req.params.mangaName;
-  console.log('got request for ' + mangaName + ' from ' + req.get('host'));
-
   const url = `https://anime-sama.fr/catalogue/${sanitizeMangaName(mangaName)}/scan/vf/episodes.js?filever=615193`;
 
   try {
     const response = await axios.get(url);
     res.json(response.data);
   } catch (error) {
-    console.log('error:' + error);
     res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
   }
 });
