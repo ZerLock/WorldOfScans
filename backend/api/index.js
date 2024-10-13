899d79d79e4482b81a3c758b433e54cdb139dd26
Léo Dubosclard <leo.dubosclard@epitech.eu>
2024-10-14 01:50:28 +0900

    add log from who is using my application in backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -11,15 +11,16 @@ function sanitizeMangaName(mangaName) {
 }
 
 app.get('/manga/:mangaName', async (req, res) => {
-  const mangaName = req.params.mangaName;
-  const url = `https://anime-sama.fr/catalogue/${sanitizeMangaName(mangaName)}/scan/vf/episodes.js?filever=615193`;
+    console.log('new request from ', (req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress));
+    const mangaName = req.params.mangaName;
+    const url = `https://anime-sama.fr/catalogue/${sanitizeMangaName(mangaName)}/scan/vf/episodes.js?filever=615193`;
 
-  try {
-    const response = await axios.get(url);
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
-  }
+    try {
+        const response = await axios.get(url);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des données.' });
+    }
 });
 
 module.exports = app;
