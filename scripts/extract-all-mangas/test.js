5c1f14be216737b0e4966e1f54413232646b8c1e
Léo Dubosclard <leo.dubosclard@epitech.eu>
2024-10-14 13:02:03 +0900

    clean scripts

--- scripts/extract-all-mangas/test.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const bootstrap = async () => {
-	const response = await axios.get('https://anime-sama.fr/s2/scans/Adabana/1/1.jpg');
-	console.log(response.status);
-};
-
-bootstrap();
