10e7a3f89d5598191bd3ae8543f96e3a0ccb7cba
Léo Dubosclard <leo.dubosclard@epitech.eu>
2024-11-14 14:59:18 +0900

    fix: uninstalled package and non use of fastify cors

--- backend/api/serverless.js
+++ backend/api/serverless.js
@@ -1,18 +1,13 @@
 "use strict";
 
-// Read the .env file.
-import * as dotenv from "dotenv";
-dotenv.config();
-
-// Require the framework
 import Fastify from "fastify";
+import cors from '@fastify/cors';
 
-// Instantiate Fastify with some config
 const app = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
+app.register(cors);
 app.register(import("../src/app.js"));
 
 export default async (req, res) => {
