ec7f02b74c8b3c7c83fdd5f965992eaeeea5e33e
Léo Dubosclard <leo.dubosclard@epitech.eu>
2025-03-25 22:14:35 +0900

    fix(engine): engine

--- engine/animesama/api/index.js
+++ engine/animesama/api/index.js
@@ -21,7 +21,14 @@ app.get('/manga/:mangaName/chapters', async (req, res) => {
     const url = `https://anime-sama.fr/catalogue/${sanitizeMangaName(mangaName)}/${typeOfScans(mangaName)}/vf/episodes.js`;
 
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                'Accept': 'application/json, text/plain, */*',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+        });
         res.json(response.data);
     } catch (error) {
         console.log(error);
